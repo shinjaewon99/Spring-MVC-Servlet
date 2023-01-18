36042aadcba4a15723b41e65cd4ec69b2dcb4c17
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-18 15:10:42 +0900

    feat : 파싱할 Json 데이터

---
+++ servlet/src/main/java/helloMVC/servlet/basic/HelloJson.java
@@ -0,0 +1,13 @@
+package helloMVC.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloJson {
+    // 파싱할 데이터
+    private String username;
+    private int age;
+
+
+}
