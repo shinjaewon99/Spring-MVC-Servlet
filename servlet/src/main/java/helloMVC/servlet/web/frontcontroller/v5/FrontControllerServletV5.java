f6792f40be96d962221d2fe42817a69c4594a74d
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-24 00:26:08 +0900

    feat : handler 매핑을 통해 유연한 컨트롤러 조회 (Object 사용)

--- servlet/src/main/java/helloMVC/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ servlet/src/main/java/helloMVC/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -2,11 +2,14 @@ package helloMVC.servlet.web.frontcontroller.v5;
 
 import helloMVC.servlet.web.frontcontroller.ModelView;
 import helloMVC.servlet.web.frontcontroller.MyView;
-import helloMVC.servlet.web.frontcontroller.v3.ControllerV3;
 import helloMVC.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import helloMVC.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import helloMVC.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import helloMVC.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import helloMVC.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import helloMVC.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import helloMVC.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import helloMVC.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,18 +27,28 @@ public class FrontControllerServletV5 extends HttpServlet {
 
     private final Map<String, Object> handlerMappingMap = new HashMap<>();
     private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
     public FrontControllerServletV5() {
         initHandlerMappingMap();
         initHandlerAdapters();
     }
+
     private void initHandlerMappingMap() {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        // V4 추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
+
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse
             response)
@@ -50,10 +63,12 @@ public class FrontControllerServletV5 extends HttpServlet {
         MyView view = viewResolver(mv.getViewName());
         view.render(mv.getModel(), request, response);
     }
+
     private Object getHandler(HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         return handlerMappingMap.get(requestURI);
     }
+
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
         for (MyHandlerAdapter adapter : handlerAdapters) {
             if (adapter.supports(handler)) {
@@ -62,6 +77,7 @@ public class FrontControllerServletV5 extends HttpServlet {
         }
         throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler=" + handler);
     }
+
     private MyView viewResolver(String viewName) {
         return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
