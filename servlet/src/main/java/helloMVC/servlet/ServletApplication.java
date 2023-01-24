94769850c3fd93792a14e595abded818dc551e5f
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-24 13:37:00 +0900

    feat : handler 매핑과 handler 어댑터를 통해 개발

--- servlet/src/main/java/helloMVC/servlet/ServletApplication.java
+++ servlet/src/main/java/helloMVC/servlet/ServletApplication.java
@@ -3,7 +3,10 @@ package helloMVC.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 /*
@@ -15,8 +18,14 @@ Servlet 찾아 등록해 실행할수있게 도와준다.
 @SpringBootApplication
 public class ServletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
+
+    @Bean
+	ViewResolver internalResourceView() {
+        return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+
+    }
 
 }
